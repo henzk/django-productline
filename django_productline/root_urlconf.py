ea5ba09db80b318ad723cefb7d75ff382cdd218a
Hendrik Speidel <speidel@schnapptack.de>
2013-02-02 23:44:28 +0100

    fixed import

--- django_productline/root_urlconf.py
+++ django_productline/root_urlconf.py
@@ -6,6 +6,6 @@ urlpatterns are constructed by refining django_productline.urls.get_urls.
 Here, get_urls is called to get the (composed) urlpatterns.
 Django uses these to construct the root RegexUrlResolver.
 """
-from featuredjango import urls
+from django_productline import urls
 
 urlpatterns = urls.get_urls()
