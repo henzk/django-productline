95ec7131229ab97015273b0fc572ec0e6cd5aff5
Hendrik Speidel <speidel@schnapptack.de>
2013-02-02 23:45:35 +0100

    removed staticfiles related stuff from settings

--- django_productline/settings.py
+++ django_productline/settings.py
@@ -47,31 +47,6 @@ MEDIA_ROOT = PRODUCT_CONTEXT.DATA_DIR + '/uploads/'
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PRODUCT_CONTEXT.DATA_DIR + '/generated_static/'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = [
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-]
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-]
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = PRODUCT_CONTEXT.SECRET_KEY
 
@@ -109,7 +84,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
