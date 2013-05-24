f68b559a31b77505a2a9ecd0f635100a03d43fad
Toni Michel <toni@Ranjid-V.fritz.box>
2013-05-24 12:12:33 +0200

    fixed issue considering a '/' admin_url; added comments;

--- django_productline/features/admin/context_processors.py
+++ django_productline/features/admin/context_processors.py
@@ -6,22 +6,24 @@ def django_admin(request):
     '''
     Adds the variable boolean django_admin to context indicating whether the current 
     page is part of the django admin or not.
+    
+    REMEMBER: do not set ADMIN_URL='/' in case your application features functionality
+    outside of django admin; otherwise all incoming urls are interpreted as admin urls.
     '''
-    
-    
-    
+    # ensure that adminurl always starts with a '/' but never ends with a '/'
     if settings.ADMIN_URL.endswith('/'):
         admin_url = ''.join(settings.ADMIN_URL[:-1])
     if not settings.ADMIN_URL.startswith('/'):
         admin_url = '/' + settings.ADMIN_URL
     
-    data = {
-        'ADMIN_URL': admin_url
-    }
-    
-    if request.META['PATH_INFO'].startswith(admin_url):
+    # add ADMIN_URL to context
+    data = {'ADMIN_URL': admin_url}
+       
+    if request.user.is_staff and request.META['PATH_INFO'].startswith(admin_url):
+        # only set django_admin in case the user is staff and the
+        # current url starts with the admin url.
         data['django_admin'] = True
     else:
         data['django_admin'] = False
-        
+    
     return data
