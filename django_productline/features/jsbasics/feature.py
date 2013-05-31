9e0565a05702b0fab805278ab0274b40e4274045
henzk <speidel@schnapptack.de>
2013-05-31 16:43:19 +0200

    removed feature jsbasics ... django-productline core should be JS-framework agnostic

--- django_productline/features/jsbasics/feature.py
+++
@@ -1,7 +0,0 @@
-
-def select(composer):
-    #compose settings
-    import django_productline.settings
-    from . import settings
-    composer.compose(settings, django_productline.settings)
-
--- django_productline/features/jsbasics/settings.py
+++
@@ -1,4 +0,0 @@
-
-
-def refine_INSTALLED_APPS(original):
-    return ['django_productline.features.jsbasics'] + list(original)
