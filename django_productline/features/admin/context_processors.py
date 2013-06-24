a1b28ae14cb22efa5c062f2a8d2f407c8b30905b
henzk <speidel@schnapptack.de>
2013-06-25 00:13:55 +0200

    refactored admin.context_processors a little; improved docstr

--- django_productline/features/admin/context_processors.py
+++ django_productline/features/admin/context_processors.py
@@ -1,30 +1,27 @@
 from django.conf import settings
 
 
-
 def django_admin(request):
     '''
-    Adds the variable boolean django_admin to context indicating whether the current 
+    Adds additional information to the context:
+
+    ``django_admin`` - boolean variable indicating whether the current
     page is part of the django admin or not.
-    
-    REMEMBER: do not set ADMIN_URL='/' in case your application features functionality
-    outside of django admin; otherwise all incoming urls are interpreted as admin urls.
+    ``ADMIN_URL`` - normalized version of settings.ADMIN_URL; starts with a slash, ends without a slash
+
+    NOTE: do not set ADMIN_URL='/' in case your application provides functionality
+    outside of django admin as all incoming urls are interpreted as admin urls.
     '''
     # ensure that adminurl always starts with a '/' but never ends with a '/'
     if settings.ADMIN_URL.endswith('/'):
-        admin_url = ''.join(settings.ADMIN_URL[:-1])
+        admin_url = settings.ADMIN_URL[:-1]
     if not settings.ADMIN_URL.startswith('/'):
         admin_url = '/' + settings.ADMIN_URL
-    
-    # add ADMIN_URL to context
-    data = {'ADMIN_URL': admin_url}
-       
-    if request.META['PATH_INFO'].startswith(admin_url):
-        # only set django_admin in case the user is staff and the
-        # current url starts with the admin url.
-        data['django_admin'] = True
-    else:
-        data['django_admin'] = False
-            
-    print data
+
+    # add ADMIN_URL and django_admin to context
+    data = {
+        'ADMIN_URL': admin_url,
+        'django_admin': request.META['PATH_INFO'].startswith(admin_url)
+    }
+
     return data
