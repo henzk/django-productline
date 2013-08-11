e7ff080e3c31aab16a82bf6d20c29e9889e278b7
tonimichel <toni@schnapptack.de>
2013-08-11 17:27:30 +0200

    removed wrong import

--- django_productline/context.py
+++ django_productline/context.py
@@ -11,7 +11,6 @@ The context is loaded from a file in json format.
 """
 import json
 import os
-from django.conf import settings
 
 PRODUCT_CONTEXT = None
 
@@ -74,7 +73,6 @@ def bind_context(context_filename):
             context['CONTAINER_DIR'] = os.environ['CONTAINER_DIR']
             context['APE_ROOT_DIR'] = os.environ['APE_ROOT_DIR']
             context['APE_GLOBAL_DIR'] = os.environ['APE_GLOBAL_DIR']
-            context['MEDIA_ROOT'] = settings.MEDIA_ROOT
             PRODUCT_CONTEXT = ContextAccessor(context)
     else:
         #bind_context called but context already bound
