26a5f988acce222b18c80781414dbed7de444787
tonimichel <toni@schnapptack.de>
2013-09-28 17:39:04 +0200

    STATIC_ROOT moved to djpl.settings.STATIC_ROOT = None; STATIC_ROOT is refined by djpl-product-data or djpl-container-data;

--- django_productline/features/staticfiles/settings.py
+++ django_productline/features/staticfiles/settings.py
@@ -3,12 +3,6 @@
 def refine_INSTALLED_APPS(original):
     return ['django.contrib.staticfiles'] + list(original)
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-from django_productline.context import PRODUCT_CONTEXT
-introduce_STATIC_ROOT = PRODUCT_CONTEXT.DATA_DIR + '/generated_static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
