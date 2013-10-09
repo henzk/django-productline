e663512dd73be4e76eb755fad52710382ad2840a
tonimichel <toni@schnapptack.de>
2013-10-09 17:48:23 +0200

    renamed prepare_static_files to collectstatic and refined deploy to invoke collectstatic

--- django_productline/features/staticfiles/tasks.py
+++ django_productline/features/staticfiles/tasks.py
@@ -2,7 +2,7 @@ from ape import tasks
 
 @tasks.register
 @tasks.requires_product_environment
-def prepare_staticfiles(force=False):
+def collectstatic(force=False):
     """
     collect static files for production httpd
 
@@ -13,15 +13,18 @@ def prepare_staticfiles(force=False):
     from django.conf import settings
     if force or not settings.DEBUG:
         tasks.manage('collectstatic')
+        print '... finished collectstatic'
+        print
+    else:
+        print '... skipping collectstatic as settings.DEBUG=True; run ape collectstatic instead;'
 
 
 
-
-def refine_post_context_deploy(original):
+def refine_deploy(original):
     """
     adds call to build_staticfiles
     """
     def deploy():
         original()
-        tasks.prepare_staticfiles()
+        tasks.collectstatic()
     return deploy
