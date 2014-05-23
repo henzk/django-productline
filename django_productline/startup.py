9540f12a22782ca6d8ecd3026f0ce4380d68b04d
henzk <speidel@schnapptack.de>
2014-05-23 17:10:57 +0200

    force registration of overextends tag library
    this fixes sporadic import error by importing get_template  ?!?!?!
    most likely a race condition of some sort
    
    see:
    https://github.com/django-compressor/django-compressor/issues/131
    https://github.com/django-compressor/django-compressor/issues/141

--- django_productline/startup.py
+++ django_productline/startup.py
@@ -10,7 +10,7 @@ _product_selected = False
 def select_product():
     """
     binds the frozen context the selected features
-    
+
     should be called only once - calls after the first call have
     no effect
     """
@@ -40,13 +40,17 @@ def select_product():
     #eager creation of URLResolver
     get_resolver(None)
 
+    #make sure overextends tag is registered
+    from django.template.loader import get_template
+    from overextends import models
+
 def get_wsgi_application():
     """
     returns the wsgi application for the selected product
-    
+
     this function is called by featuredjango.wsgi to get the wsgi
     application object
-    
+
     if you need to refine the wsgi application object e.g. to add
     wsgi middleware please refine django.core.wsgi.get_wsgi_application directly.
     """
