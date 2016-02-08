74cfe6b73d3c0359ad7cb03e421802e8f6aaf9eb
Toni Michel <tonimichel@Tonis-iMac.fritz.box>
2016-02-08 08:52:41 +0100

    added contextprocessor to TEMPLATES in order to allow compativbility with django 1.9

--- django_productline/features/djpladmin/settings.py
+++ django_productline/features/djpladmin/settings.py
@@ -2,15 +2,23 @@
 
 def refine_INSTALLED_APPS(original):
     return ['django_productline.features.djpladmin', 'django.contrib.admin', ] + list(original)
-    
-    
+
+
 introduce_ADMIN_URL = 'admin/'
 
 def refine_TEMPLATE_CONTEXT_PROCESSORS(original):
-    return list(original) + ['django_productline.features.djpladmin.context_processors.django_admin']
-    
-    
-    
+    return list(original) + []
+
+
+
+def refine_TEMPLATES(original):
+    OPTIONS = original[0]['OPTIONS']
+    OPTIONS['context_processors'] += [
+        'django_productline.features.djpladmin.context_processors.django_admin'
+    ]
+    return original
+
+
 
 introduce_AUTH_GROUPS = {
     #{
@@ -19,6 +27,5 @@ introduce_AUTH_GROUPS = {
     #        ('add_mymodel', 'myapp'),
     #        ('change_my_model', 'myapp')
     #    ]
-    #}    
+    #}
 }
-
