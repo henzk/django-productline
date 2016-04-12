4eba78dff11729f8bca604eec6dd74a96dcb1329
WiRai <Raiko.Wielk@googlemail.com>
2016-04-12 10:51:51 +0200

    update urls.py to not longer use patters because of deprecation and do not use string imports for the same reason

--- django_productline/features/staticfiles/urls.py
+++ django_productline/features/staticfiles/urls.py
@@ -9,15 +9,16 @@ def refine_get_urls(original):
     """
 
     def get_urls():
-        from django.conf.urls import patterns, include, url
+        from django.conf.urls import url
         from django.conf import settings
         from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+        from django.views.static import serve
         if settings.DEBUG:
-            return staticfiles_urlpatterns() + patterns('',
-                url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            return staticfiles_urlpatterns() + [
+                url(r'^media/(?P<path>.*)$', serve, {
                     'document_root': settings.MEDIA_ROOT,
                 }),
-            ) + original()
+            ] + original()
         else:
             return original()
     return get_urls
