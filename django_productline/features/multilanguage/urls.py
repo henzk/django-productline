edb7233dca34bb0eda56cda067c50fe944aeb513
Dominik Halle <dominik.halle@schnapptack.de>
2017-04-24 18:12:56 +0200

    added missing import

--- django_productline/features/multilanguage/urls.py
+++ django_productline/features/multilanguage/urls.py
@@ -13,6 +13,7 @@ def refine_get_urls(original):
     def get_urls():
         from django_productline.urls import get_multilang_urls
         from django.conf.urls.i18n import i18n_patterns
+        from django.conf.urls import url, include
         from django.contrib import admin
         from django.conf import settings
         return original() + i18n_patterns(*get_multilang_urls(), prefix_default_language=settings.PREFIX_DEFAULT_LANGUAGE) + i18n_patterns(url(r'^%s' % settings.ADMIN_URL, include(admin.site.urls)))
