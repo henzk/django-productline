b31665775f5469bf7aea095b130ee1cf433ad3fb
Dominik Halle <dominik.halle@schnapptack.de>
2017-04-25 11:13:17 +0200

    removed setting refinement

--- django_productline/features/multilanguage_admin/feature.py
+++ django_productline/features/multilanguage_admin/feature.py
@@ -3,9 +3,6 @@ from __future__ import unicode_literals
 
 
 def select(composer):
-    from . import settings
-    import django_productline.settings
-    composer.compose(settings, django_productline.settings)
 
     from . import admin_urls_refinement
     import django_productline.features.djpladmin.admin_urls
